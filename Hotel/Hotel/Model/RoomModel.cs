ea91ea1ce8c7c96ea9a8c1399f296a559475091e
VoDinhPhuc <melonation.tloss21@gmail.com>
2022-12-14 14:31:10 +0700

    add exeptions, model

---
+++ Hotel/Hotel/Model/RoomModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Model
+{
+    public class RoomModel
+    {
+        public int FloorNumber { get; }
+        public int RoomNumber { get; }
+        public bool IsTheSameRoomAs(RoomModel room) { return room.FloorNumber == FloorNumber && room.RoomNumber == RoomNumber; }
+        public override string ToString()
+        {
+            return $"{FloorNumber}{RoomNumber}";
+        }
+    }
+
+    
+}
