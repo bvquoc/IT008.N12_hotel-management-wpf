d84b5cedf5209a6282b20688dac66c787924209a
Khooiiiii <21521010@gm.edu.vn>
2022-12-19 12:45:08 +0700

    make service

---
+++ Hotel/Hotel/ViewModel/ServiceVM.cs
@@ -0,0 +1,32 @@
+﻿using Hotel.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Hotel.ViewModel
+{
+    internal class ServiceVM : BaseViewModel
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public ICommand ShowMessage { get; set; }
+        public ServiceVM()
+        {
+            ShowMessage = new RelayCommand<RoomView>((parameter) => true, (parameter) =>
+            {
+                MessageBox.Show(this.ID);
+            });
+        }
+        public ServiceVM(string id, string name, int price)
+        {
+            Name = name;
+            ID=id;
+            price =price;
+        }
+    }
+}
---
+++ Hotel/Hotel/ViewModel/ServicesViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Hotel.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Hotel.ViewModel
+{
+    internal class ServiceViewModel : BaseViewModel
+    {
+        private ObservableCollection<ServiceVM> _ServiceList;
+        public ObservableCollection<ServiceVM> ServiceList
+        {
+            get { return _ServiceList; }
+            set { _ServiceList = value; OnPropertyChanged(); }
+        }
+        public ServiceViewModel()
+        {
+            ServiceList = new ObservableCollection<ServiceVM>();
+            LoadDemo();
+        }
+
+        public void LoadDemo()
+        {
+            ServiceList.Add(new ServiceVM() { ID = "B101", Name = "Mì", Price = 1003340 });
+            ServiceList.Add(new ServiceVM() { ID = "B102", Name = "Bánh", Price = 10000 });
+            ServiceList.Add(new ServiceVM() { ID = "B103", Name = "Buffe", Price = 10000 });
+            ServiceList.Add(new ServiceVM() { ID = "B104", Name = "Hồ bơi", Price = 10000 });
+            ServiceList.Add(new ServiceVM() { ID = "B105", Name = "Karaoke", Price = 10000 });
+            ServiceList.Add(new ServiceVM() { ID = "B106", Name = "Massage", Price = 10000 });
+            ServiceList.Add(new ServiceVM() { ID = "B107", Name = "18+", Price = 10000 });
+            ServiceList.Add(new ServiceVM() { ID = "B108", Name = "Cinema", Price = 10000 });
+            ServiceList.Add(new ServiceVM() { ID = "B109", Name = "Video game", Price = 10000 });
+
+        }
+    }
+
+}
