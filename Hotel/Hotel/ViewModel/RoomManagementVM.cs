eeb4ef0836970c063aec91720d72007afa330758
Khooiiiii <21521010@gm.edu.vn>
2022-12-24 17:49:51 +0700

    make thing

---
+++ Hotel/Hotel/ViewModel/RoomManagementVM.cs
@@ -0,0 +1,34 @@
+﻿using Hotel.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Hotel.ViewModel
+{
+    internal class RoomManagementVM : BaseViewModel
+    {
+        public string ID { get; set; }
+        public string Status { get; set; }
+        public int Price { get; set; }
+        public string Type { get; set; }
+        public ICommand ShowMessage { get; set; }
+        public RoomManagementVM()
+        {
+            ShowMessage = new RelayCommand<RoomView>((parameter) => true, (parameter) =>
+            {
+                MessageBox.Show(this.ID);
+            });
+        }
+        public RoomManagementVM(string id, string status, int price, string type)
+        {
+            ID = id;
+            Type = type;
+            Status = status;
+            Price = price;
+        }
+    }
+}
---
+++ Hotel/Hotel/ViewModel/RoomManagementViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Hotel.Model;
+using Hotel.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Hotel.ViewModel
+{
+    internal class RoomManagementViewModel : BaseViewModel
+    {
+        private ObservableCollection<RoomManagementVM> _RoomManagementList;
+        public ObservableCollection<RoomManagementVM> RoomManagementList
+        {
+            get { return _RoomManagementList; }
+            set { _RoomManagementList = value; OnPropertyChanged(); }
+        }
+        private string _textToFilterSV;
+
+        public string TextToFilterSV
+        {
+            get { return _textToFilterSV; }
+            set
+            {
+                _textToFilterSV = value;
+                OnPropertyChanged();
+                RoomManagementCollection.Filter = FilterByName;
+            }
+        }
+        private ICollectionView _RoomManagementCollection;
+
+        public ICollectionView RoomManagementCollection
+        {
+            get { return _RoomManagementCollection; }
+            set { _RoomManagementCollection = value; OnPropertyChanged(); }
+        }
+        public RoomManagementViewModel()
+        {
+            RoomManagementList = new ObservableCollection<RoomManagementVM>();
+            //LoadDemo();
+            LoadAllSV();
+            RoomManagementCollection = CollectionViewSource.GetDefaultView(RoomManagementList);
+        }
+        private bool FilterByName(object emp)
+        {
+            if (!string.IsNullOrEmpty(TextToFilterSV))
+            {
+                var empDetail = emp as RoomManagementVM;
+                return empDetail != null && empDetail.ID.IndexOf(TextToFilterSV, StringComparison.OrdinalIgnoreCase) >= 0;
+            }
+            return true;
+        }
+        public void LoadAllSV()
+        {
+            using (var db = new QLYHOTELEntities())
+            {
+                var select = from s in db.PHONGs select s;
+                foreach (var RoomManagement in select)
+                    RoomManagementList.Add(new RoomManagementVM() { ID = RoomManagement.TENPHONG.ToString(), Status = RoomManagement.TRANGTHAI.ToString(), Type = RoomManagement.LOAIPHONG.ToString(),Price=RoomManagement.DONGIA.Value });
+            }
+        }
+      
+    }
+
+}
