adc1692c69a023a650ebfa44649516ca2648a347
Son <sontruong02003@gmail.com>
2023-01-10 21:36:36 +0700

    up

--- Hotel/Hotel/ViewModel/AddEmployeeViewModel.cs
+++ Hotel/Hotel/ViewModel/AddEmployeeViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using Hotel.Model;
 using Hotel.View;
+using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Hotel.ViewModel
 {
@@ -16,33 +19,87 @@ namespace Hotel.ViewModel
         }
         private void saveCustomer(AddEmployee p)
         {
-            var employee = new NHANVIEN();
-            employee.TENNV = p._CCCD.Text;
-            employee.CCCD = p._CCCD.Text;
-            employee.NGSINH = p._BirthDay.SelectedDate.Value;
-            employee.SDT = p._SDT.Text;
-            employee.TAIKHOAN = p._Account.Text;
-            employee.MATKHAU = p._Password.Password.ToString();
-            string LoaiNv = p.cbType.SelectionBoxItem.ToString();
-            switch (LoaiNv)
+            try
             {
-                case "Quản lý":
-                    employee.LOAINV = 1;
-                    break;
-                case "Nhân viên":
-                    employee.LOAINV = 2;
-                    break;
-                default:
-                    break;
+                var employee = new NHANVIEN();
+                if (p._Name.Text == "")
+                    throw new Exception("Chưa nhập tên!");
+                employee.TENNV = p._Name.Text;
+
+                if (p._CCCD.Text == "")
+                    throw new Exception("Chưa nhập CMND/CCCD!");
+                if (checkCMND(p._CCCD.Text))
+                    throw new Exception("CMND/CCCD không hợp lệ!");
+                employee.CCCD = p._CCCD.Text;
+
+                if (p._BirthDay.SelectedDate == null)
+                    throw new Exception("Ngày sinh không hợp lệ!");
+                employee.NGSINH = p._BirthDay.SelectedDate.Value;
+
+                if (p._SDT.Text == "")
+                    throw new Exception("Chưa nhập số điện thoại!");
+                if (checkSDT(p._SDT.Text))
+                    throw new Exception("Số điện thoại không hợp lệ!");
+                employee.SDT = p._SDT.Text;
+
+                if (p._Account.Text == "")
+                    throw new Exception("Chưa nhập tài khoản!");
+                if (checkAccount(p._Account.Text))
+                    throw new Exception("Tài khoản đã tồn tại!");
+                employee.TAIKHOAN = p._Account.Text;
+
+                if (p._Password.Password == "")
+                    throw new Exception("Chưa nhập mật khẩu!");
+                employee.MATKHAU = p._Password.Password;
+
+                string LoaiNv = p.cbType.SelectionBoxItem.ToString();
+                switch (LoaiNv)
+                {
+                    case "Quản lý":
+                        employee.LOAINV = 1;
+                        break;
+                    case "Nhân viên":
+                        employee.LOAINV = 2;
+                        break;
+                    default:
+                        throw new Exception("Chức vụ không hợp lê!");
+                        break;
+                }
+                employee.SONGAYLV = 0;
+                employee.LUONG = 1;
+                using (var db = new QLYHOTELEntities())
+                {
+                    db.NHANVIENs.Add(employee);
+                    db.SaveChanges();
+                }
+                new DialogCustomize("Thêm thành công!").ShowDialog();
+                //Refresh
             }
-            employee.SONGAYLV = 0;
-            employee.LUONG = 1;
-            using (var db = new QLYHOTELEntities())
+            catch (Exception ex)
             {
-                db.NHANVIENs.Add(employee);
-                db.SaveChanges();
+                new DialogCustomize(ex.Message).ShowDialog();
             }
         }
-
+        private bool checkAccount(string account)
+        {
+            using (var db = new QLYHOTELEntities())
+            {
+                var check = from n in db.NHANVIENs select n;
+                foreach (var p in check)
+                    if (p.TAIKHOAN == account)
+                        return true;
+            }
+            return false;
+        }
+        private bool checkCMND(string cmnd)
+        {
+            if (cmnd.Length == 9 || cmnd.Length == 12)
+                return false;
+            return true;
+        }
+        private bool checkSDT(string number)
+        {
+            return !(number.Length == 10);
+        }
     }
 }
