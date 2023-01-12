a107f0ca661957d2036f4a67c093dc1d900b3657
Son <sontruong02003@gmail.com>
2023-01-12 15:26:54 +0700

    add Notification

--- Hotel/Hotel/ViewModel/NavigationViewModel.cs
+++ Hotel/Hotel/ViewModel/NavigationViewModel.cs
@@ -9,6 +9,8 @@ namespace Hotel.ViewModel
     {
         private double FontBase = 12;
         private double _isHome;
+        private string bell = "/Images/Bell.png";
+        private string bellActive = "/Images/Active.png";
 
         public double IsHome
         {
@@ -69,67 +71,27 @@ namespace Hotel.ViewModel
             set { _currentView = value; OnPropertyChanged(); }
         }
 
+        private string _notification;
+
+        public string Notification
+        {
+            get { return _notification; }
+            set { _notification = value; OnPropertyChanged(); }
+        }
+
         public ICommand LogoutCommand { get; set; }
         public ICommand GetUidCommand { get; set; }
         public ICommand MakeNavigation { get; set; }
+        public ICommand GetNotification { get; set; }
         private string uid = "0";
 
         public NavigationViewModel()
         {
-            LogoutCommand = new RelayCommand<Window>((parameter) => true, (parameter) =>
-            {
-                parameter.Hide();
-                LoginView login = new LoginView();
-                login.ShowDialog();
-                parameter.Close();
-            });
-
+            LogoutCommand = new RelayCommand<Window>((parameter) => true, (parameter) => logout(parameter));
             GetUidCommand = new RelayCommand<Button>((parameter) => true, (parameter) => uid = parameter.Uid);
+            MakeNavigation = new RelayCommand<Button>((parameter) => true, (parameter) => makeNavigation(parameter));
+            GetNotification = new RelayCommand<object>((parameter) => true, (parameter) => readNotification());
 
-            MakeNavigation = new RelayCommand<Button>((parameter) => true, (parameter) =>
-            {
-                IsHome = FontBase;
-                IsReservation = FontBase;
-                IsCustomer = FontBase;
-                IsRoomManagement = FontBase;
-                IsBill = FontBase;
-                IsServiceManagement = FontBase;
-                IsEmployeeManagement = FontBase;
-
-                switch (uid)
-                {
-                    case "0":
-                        IsHome = FontBase + 1;
-                        CurrentView = new RoomView();
-                        break;
-                    case "1":
-                        IsReservation = FontBase + 1;
-                        CurrentView = new ReservationBookView();
-                        break;
-                    case "2":
-                        IsCustomer = FontBase + 1;
-                        CurrentView = new CustomerManagementView();
-                        break;
-                    case "3":
-                        IsRoomManagement = FontBase + 1;
-                        CurrentView = new RoomManagementView();
-                        break;
-                    case "4":
-                        IsBill = FontBase + 1;
-                        CurrentView = new BillView();
-                        break;
-                    case "5":
-                        IsServiceManagement = FontBase + 1;
-                        CurrentView = new ServiceManagementView();
-                        break;
-                    case "6":
-                        IsEmployeeManagement = FontBase + 1;
-                        CurrentView = new EmployeeManagementView();
-                        break;
-                    default:
-                        break;
-                }
-            });
             IsHome = FontBase + 1;
             IsReservation = FontBase;
             IsCustomer = FontBase;
@@ -139,6 +101,67 @@ namespace Hotel.ViewModel
             IsEmployeeManagement = FontBase;
 
             CurrentView = new RoomView();
+
+            //testc bell
+            Notification = bell;
+        }
+        private void makeNavigation(Button parameter)
+        {
+            IsHome = FontBase;
+            IsReservation = FontBase;
+            IsCustomer = FontBase;
+            IsRoomManagement = FontBase;
+            IsBill = FontBase;
+            IsServiceManagement = FontBase;
+            IsEmployeeManagement = FontBase;
+
+            switch (uid)
+            {
+                case "0":
+                    IsHome = FontBase + 1;
+                    CurrentView = new RoomView();
+                    break;
+                case "1":
+                    IsReservation = FontBase + 1;
+                    CurrentView = new ReservationBookView();
+                    break;
+                case "2":
+                    IsCustomer = FontBase + 1;
+                    CurrentView = new CustomerManagementView();
+                    break;
+                case "3":
+                    IsRoomManagement = FontBase + 1;
+                    CurrentView = new RoomManagementView();
+                    break;
+                case "4":
+                    IsBill = FontBase + 1;
+                    CurrentView = new BillView();
+                    break;
+                case "5":
+                    IsServiceManagement = FontBase + 1;
+                    CurrentView = new ServiceManagementView();
+                    break;
+                case "6":
+                    IsEmployeeManagement = FontBase + 1;
+                    CurrentView = new EmployeeManagementView();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void logout(Window parameter)
+        {
+            parameter.Hide();
+            LoginView login = new LoginView();
+            login.ShowDialog();
+            parameter.Close();
+        }
+        private void readNotification()
+        {
+            if (Notification == bell)
+                Notification = bellActive;
+            else
+                Notification = bell;
         }
     }
 }
