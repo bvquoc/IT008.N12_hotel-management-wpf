6f887ebf0203db7715d5272ac42ecd8d509d903f
Khooiiiii <21521010@gm.edu.vn>
2023-01-23 15:39:35 +0700

    add bill detail

---
+++ Hotel/Hotel/View/BillDetail.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hotel.View
+{
+    /// <summary>
+    /// Interaction logic for BillDetail.xaml
+    /// </summary>
+    public partial class BillDetail : Window
+    {
+        public BillDetail()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.IsEnabled = false;
+                PrintDialog printDialog = new PrintDialog();
+                if (printDialog.ShowDialog() == true)
+                {
+                    printDialog.PrintVisual(print, "invoice");
+                }
+            }
+            finally
+            {
+                this.IsEnabled = true;
+            }
+        }
+    }
+}
