91a19ddceb6fa3d4a5d4a3a410b80c871b843eaf
Khooiiiii <21521010@gm.edu.vn>
2023-01-27 12:54:08 +0700

    Thêm vài thứ

---
+++ Hotel/Hotel/View/CustomerDetail.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hotel.View
+{
+    /// <summary>
+    /// Interaction logic for CustomerDetail.xaml
+    /// </summary>
+    public partial class CustomerDetail : Window
+    {
+        public CustomerDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
